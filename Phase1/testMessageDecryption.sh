cd023222e715b339d2f5f7efd579d7dab9443bf6
Joseph <jcastelan02@gmail.com>
2018-02-27 17:24:33 -0800

    added the file to correctly compile messageDecryption

--- Phase1/testMessageDecryption.sh
+++ Phase1/testMessageDecryption.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-#compiling
+#compiling (https://stackoverflow.com/questions/34368804/what-are-the-link-options-lcrypto-and-lssl-in-compiling-programs-with-openss)
 gcc -c messageDecryption.c
 gcc -c crypto.c
-gcc -lssl -lcrypto -o messageDecryption crypto.o messageDecryption.o 
+gcc -o messageDecryption crypto.o messageDecryption.o -L /usr/local/ssl/lib -lcrypto -lssl -ldl
 
 #testing
 ./messageDecryption message.cipher iv.plain key.plain
+
+#removing junk
+rm *.o
 
 echo TESTING DONE
